31e58088a26b02f97b5372d498364ae387cceb45
Lars F. Karlström <1514950+lfk@users.noreply.github.com>
2014-09-24 03:03:28 -0700

    Added basic queries to statistics class, table view (and flexbox) on the estad?sticas page

--- pages/estadisticas.php
+++ pages/estadisticas.php
@@ -9,7 +9,6 @@
 	$err = '';
 
 
-
 	foreach($data as $age => $entry) {
 		$avg .= '[' . $age . ', ' . $entry['average'] . '], ';
 		$err .= '[' . ($entry['average'] - 2 * $entry['std_dev']) . ', ' . ($entry['average'] + 2 * $entry['std_dev'])  . '], '; // Standard deviation
@@ -115,4 +114,27 @@
 
 <h1><img src="resources/img/icon-statistics.png" height="48" width="48" alt=""> Estadísticas</h1>
 
-<div id="avg_wpm_chart" class="chart" style="width: 100%; height: 450px;"></div>
+<div class="fb-grid">
+	<div id="general-stats" class="box">
+		<h3>Datos generales</h3>
+		<?php $data = $statistics->getBasicStats(); ?>
+		<table class="noeffects" style="width: 90%;"> <!--; position: absolute; top: 10px; right: 10px;">-->
+			<tr><th rowspan="2">Totales</th><td># Pacientes</td><td><?php echo number_format($data['tot_patients']); ?></td></tr>
+			<tr><td># Expedientes</td><td><?php echo number_format($data['tot_records']); ?></td></tr>
+			<tr><td colspan="3">&nbsp;</td></tr>
+			<tr><th rowspan="3">Promedios</th><td>Expedientes por paciente</td><td><?php printf('%.2f', $data['avg_records_per_patient']); ?></td></tr>
+			<tr><td>Nuevos pacientes por día</td><td><?php printf('%.2f', $data['avg_patients_per_day']); ?></td></tr>
+			<tr><td>Expedientes registrados por día</td><td><?php printf('%.2f', $data['avg_records_per_day']); ?></td></tr>
+		</table>
+	</div>
+
+	<div id="avg_wpm_chart" class="chart" style="height: 300px;"></div>
+</div>
+<div class="fb-grid">
+	<div class="box">
+		Histograma tonometría
+	</div>
+	<div class="box">
+		Promedio tonometría por investigación
+	</div>
+</div>
