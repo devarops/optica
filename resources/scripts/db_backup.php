0314c472c84187f7b77cf19bb6a04ceefd7adb04
Lars F. Karlström <1514950+lfk@users.noreply.github.com>
2014-09-26 22:13:52 -0700

    DB backup feature implemented

---
+++ resources/scripts/db_backup.php
@@ -0,0 +1,24 @@
+<?php
+	require_once('../../db_connect.php'); // For $db_ variables
+	ob_start();
+
+	$fn  = 'db_opticahorus_' . date('Y-m-d_H-i-s') . '.sql';
+	$cmd = '/usr/bin/mysqldump --add-drop-table --host=' . $db_host . ' --user=' . $db_user .
+		' --password=' . $db_pwd . ' ' . $db_name;// . ' > ' . $fn;
+
+	system($cmd);
+
+	$dump = ob_get_contents();
+	ob_end_clean();
+
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Disposition: attachment; filename=' . $fn);
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($fn));
+	flush();
+	echo $dump;
+	exit;
+?>
