87c029b128c01fe6679e7d37ee7927e9ae27f6b9
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2015-11-04 22:23:28 -0800

    Cambia el nombre de la base de datos de opticahorus a optica por simplicidad

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 	// Alter username and password, then save this file as db_connect.php
-	$db_name = 'opticahorus';
+	$db_name = 'optica';
 	$db_host = 'localhost';
-	$db_user = 'USERNAME';
-	$db_pass = 'PASSWORD';
+	$db_user = 'optica';
+	$db_pass = 'Horus';
 
 	try {
 		$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass, array(PDO::ATTR_PERSISTENT => true, PDO::ERRMODE_EXCEPTION => true));
--- resources/scripts/backup_horus.sh
+++ resources/scripts/backup_horus.sh
@@ -3,6 +3,6 @@ UN="optica"
 PW="Horus"
 DEST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../dbdumps"
 DATE="$(date +"%Y-%m-%d")"
-FILE="$DEST/opticahorus_$DATE.sql"
+FILE="$DEST/optica_$DATE.sql"
 mkdir -p $DEST
-mysqldump -u$UN -p$PW opticahorus > $FILE
+mysqldump -u$UN -p$PW optica > $FILE
